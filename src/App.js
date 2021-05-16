22ae8e3d7863188fc2551e9554451ec0b12bba94
Sukina AbuHammad <sukina_89@yahoo.com>
2021-05-16 15:40:22 +0300

    logOut done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import BestBooks from './BestBooks';
 import Login from './Login';
+
 import Profile from './profile';
 import { withAuth0 } from "@auth0/auth0-react";
 
@@ -37,7 +38,7 @@ class App extends React.Component {
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
               <Route exact path="/profile">
                 <Profile />
-                </Route>
+              </Route>
             </Switch>
             <Footer />
           </IsLoadingAndError>
