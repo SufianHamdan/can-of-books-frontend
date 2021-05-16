c41bdf55f9d3ffb347244e36e2c57f2a216ec48d
SufianHamdan <sufian_hamdan@yahoo.com>
2021-05-16 14:17:23 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-yobojlht.eu.auth0.com"
+    clientId="BM3e0KqEvUxrZvXTNaWoAUYiCKQEnlFy"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
